37adaafbfbd87708f6847a34d18ccf537fad41fc
Ashr0botely <ashaarcher256@gmail.com>
2025-10-22 08:34:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-7mm247jh_helpinghand
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Ashr0botely/stackblitz-starters-7mm247jh_helpinghand)
---
+++ script.js
@@ -0,0 +1,26 @@
+function displayResult(){
+
+  var sport1 = document.getElementById("mySport").value;
+  document.getElementById("myResult1").innerHTML = "Your Sport is " + sport1 + ".";
+
+
+}
+ 
+
+// function showSelection(){
+
+//   const dropdown = document.getElementById("services-select").value;
+//   const selectedText = dropdown.valueOf;
+//   document.getElementById("output").textContent = "You are looking for " + selectedText + "services";
+
+
+
+
+
+ 
+  //create a variable that stores the user's input for the adjective with the id name myAdj
+  //var adj1 = document.getElementById('myAdj').value;
+  //display saying 'Your adjective is ' + adjective + '.'
+  //under the p tag with the id name adjResult
+  //ocument.getElementById('adjResult').innerHTML =
+    //'Your adjective is ' + adj1 + '.';
